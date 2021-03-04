747636a960131e1d848b3cd6fe3eabffd90a61b1
Landon Kleinbrodt <Landon.Kleinbrodt@capgroup.com>
2021-03-03 17:22:47 -0800

    finished course 2

--- AdvancedPortfolioConstruction/edhec_risk_kit_206.py
+++ AdvancedPortfolioConstruction/edhec_risk_kit_206.py
@@ -708,3 +708,105 @@ def weight_erc(r, cov_estimator=sample_cov, **kwargs):
     """
     est_cov = cov_estimator(r, **kwargs)
     return equal_risk_contributions(est_cov)
+
+def implied_returns(delta, sigma, w):
+    """
+Obtain the implied expected returns by reverse engineering the weights
+Inputs:
+delta: Risk Aversion Coefficient (scalar)
+sigma: Variance-Covariance Matrix (N x N) as DataFrame
+    w: Portfolio weights (N x 1) as Series
+Returns an N x 1 vector of Returns as Series
+    """
+    ir = delta * sigma.dot(w).squeeze() # to get a series from a 1-column dataframe
+    ir.name = 'Implied Returns'
+    return ir
+
+
+def as_colvec(x):
+    if (x.ndim == 2):
+        return x
+    else:
+        return np.expand_dims(x, axis=1)
+
+
+# Assumes that Omega is proportional to the variance of the prior
+def proportional_prior(sigma, tau, p):
+    """
+    Returns the He-Litterman simplified Omega
+    Inputs:
+    sigma: N x N Covariance Matrix as DataFrame
+    tau: a scalar
+    p: a K x N DataFrame linking Q and Assets
+    returns a P x P DataFrame, a Matrix representing Prior Uncertainties
+    """
+    helit_omega = p.dot(tau * sigma).dot(p.T)
+    # Make a diag matrix from the diag elements of Omega
+    return pd.DataFrame(np.diag(np.diag(helit_omega.values)),index=p.index, columns=p.index)
+
+
+from numpy.linalg import inv
+
+def bl(w_prior, sigma_prior, p, q,
+                omega=None,
+                delta=2.5, tau=.02):
+    """
+# Computes the posterior expected returns based on 
+# the original black litterman reference model
+#
+# W.prior must be an N x 1 vector of weights, a Series
+# Sigma.prior is an N x N covariance matrix, a DataFrame
+# P must be a K x N matrix linking Q and the Assets, a DataFrame
+# Q must be an K x 1 vector of views, a Series
+# Omega must be a K x K matrix a DataFrame, or None
+# if Omega is None, we assume it is
+#    proportional to variance of the prior
+# delta and tau are scalars
+    """
+    if omega is None:
+        omega = proportional_prior(sigma_prior, tau, p)
+    # Force w.prior and Q to be column vectors
+    # How many assets do we have?
+    N = w_prior.shape[0]
+    # And how many views?
+    K = q.shape[0]
+    # First, reverse-engineer the weights to get pi
+    pi = implied_returns(delta, sigma_prior,  w_prior)
+    # Adjust (scale) Sigma by the uncertainty scaling factor
+    sigma_prior_scaled = tau * sigma_prior  
+    # posterior estimate of the mean, use the "Master Formula"
+    # we use the versions that do not require
+    # Omega to be inverted (see previous section)
+    # this is easier to read if we use '@' for matrixmult instead of .dot()
+    #     mu_bl = pi + sigma_prior_scaled @ p.T @ inv(p @ sigma_prior_scaled @ p.T + omega) @ (q - p @ pi)
+    mu_bl = pi + sigma_prior_scaled.dot(p.T).dot(inv(p.dot(sigma_prior_scaled).dot(p.T) + omega).dot(q - p.dot(pi).values))
+    # posterior estimate of uncertainty of mu.bl
+#     sigma_bl = sigma_prior + sigma_prior_scaled - sigma_prior_scaled @ p.T @ inv(p @ sigma_prior_scaled @ p.T + omega) @ p @ sigma_prior_scaled
+    sigma_bl = sigma_prior + sigma_prior_scaled - sigma_prior_scaled.dot(p.T).dot(inv(p.dot(sigma_prior_scaled).dot(p.T) + omega)).dot(p).dot(sigma_prior_scaled)
+    return (mu_bl, sigma_bl)
+
+
+# for convenience and readability, define the inverse of a dataframe
+def inverse(d):
+    """
+    Invert the dataframe by inverting the underlying matrix
+    """
+    return pd.DataFrame(inv(d.values), index=d.columns, columns=d.index)
+
+def w_msr(sigma, mu, scale=True):
+    """
+    Optimal (Tangent/Max Sharpe Ratio) Portfolio weights
+    by using the Markowitz Optimization Procedure
+    Mu is the vector of Excess expected Returns
+    Sigma must be an N x N matrix as a DataFrame and Mu a column vector as a Series
+    This implements page 188 Equation 5.2.28 of
+    "The econometrics of financial markets" Campbell, Lo and Mackinlay.
+    """
+    w = inverse(sigma).dot(mu)
+    if scale:
+        w = w/sum(w) # fix: this assumes all w is +ve
+    return w
+
+def w_star(delta, sigma, mu):
+    return (inverse(sigma).dot(mu))/delta
+
